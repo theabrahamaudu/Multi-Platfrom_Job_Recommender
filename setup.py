3a2f9e1c0f6436c66b94231183ec517cc87368ba
Abraham Audu <ab6ril@gmail.com>
2023-10-29 20:43:15 +0100

    Initial commit

---
+++ setup.py
@@ -0,0 +1,10 @@
+from setuptools import find_packages, setup
+
+setup(
+    name='src',
+    packages=find_packages(),
+    version='0.1.0',
+    description='A system to recommend jobs scrapped from multiple job platforms with the aid of NLP techniques',
+    author='Abraham_Audu',
+    license='MIT',
+)
