a537646151205acfd0d066a7cea012af3c206572
Abraham Audu <ab6ril@gmail.com>
2023-11-01 17:10:59 +0100

    Jobberman scraper working

--- backend/etl/extract/site_scraper.py
+++ backend/etl/extract/site_scraper.py
@@ -119,7 +119,7 @@ class IndeedScraper(SiteScraper):
             self.get_job_details(self.job_link, start_at)
             logger.info(f"Scraped {len(self.job_link)} jobs")
 
-    def get_jobs(self, jobs):
+    def get_jobs(self, jobs: list):
 
         logger.info(f"Parsing {len(jobs)} job cards")
         n = 0
@@ -359,6 +359,176 @@ class LinkedinScraper(IndeedScraper):
 
             except Exception as e:
                 print('There was an error scraping Page ', page, ': ', e)
+                self.job_desc.append("NA")
+                self.seniority.append("NA")
+                self.emp_type.append("NA")
+                self.job_func.append("NA")
+                self.ind.append("NA")
+
+
+class JobbermanScraper(IndeedScraper):
+    def __init__(self,
+                 driver_path: str = "/usr/local/bin/geckodriver",
+                 url: str = "https://www.jobberman.com/jobs",
+                 num_jobs: int = 25):
+        super().__init__(driver_path, url, num_jobs)
+
+    def scrape(self):
+        logger.info(f"Scraping {self.num_jobs} jobs from Jobberman")
+        num_of_pages = ceil(self.num_jobs / 14)
+
+        for i in range(num_of_pages):
+            i = i + 1
+            page = "?page=" + str(i)
+            url = self.url + page
+            
+            wd = self.driver
+            wd.get(url)
+            jobs_lists = wd.find_element(By.XPATH, "/html/body/main/section/div[2]/div[2]/div[1]")
+            jobs = jobs_lists.find_elements(By.CLASS_NAME, "mx-5")  # return a list
+
+            # get job cards
+            self.get_jobs(jobs)
+
+            if i == 1:
+                start_at = 0
+            else:
+                start_at = 14 * (i - 1)
+
+            self.get_job_details(self.job_link, start_at)
+
+    def get_jobs(self, jobs: list):
+        n = 0
+        for job in jobs:
+            n += 1
+            try:
+                if n < 5:
+                    job_title0 = job.find_element(By.XPATH,
+                                                f'/html/body/main/section/div[2]/div[2]/div[1]/div[{n}]/div[1]/div[2]/div/div[1]/a/p').get_attribute(
+                        'innerText')
+                    self.job_title.append(job_title0)
+
+                    company_name0 = job.find_element(By.XPATH,
+                                                    f'/html/body/main/section/div[2]/div[2]/div[1]/div[{n}]/div[1]/div[2]/div/p[1]').get_attribute(
+                        'innerText')
+                    self.company_name.append(company_name0)
+
+                    location0 = job.find_element(By.XPATH,
+                                                f'/html/body/main/section/div[2]/div[2]/div[1]/div[{n}]/div[1]/div[2]/div/div[2]/span[1]').get_attribute(
+                        'innerText')
+                    self.location.append(location0)
+
+                    date0 = job.find_element(By.XPATH,
+                                            f'/html/body/main/section/div[2]/div[2]/div[1]/div[{n}]/div[2]/p').get_attribute(
+                        'innerText')
+                    self.date.append(date0)
+
+                    job_link0 = job.find_element(By.XPATH,
+                                                f'/html/body/main/section/div[2]/div[2]/div[1]/div[{n}]/div[1]/div[2]/div/div[1]/a').get_attribute(
+                        'href')
+                    self.job_link.append(job_link0)
+                    self.uuid.append(self.generate_uuid(job_link0))
+
+                    job_id0 = "Not available on Jobberman"
+                    self.job_id.append(job_id0)
+
+                else:
+                    job_title0 = job.find_element(By.XPATH,
+                                                f'/html/body/main/section/div[2]/div[2]/div[1]/div[{n}]/div[1]/div/div/div[1]/a/p').get_attribute(
+                        'innerText')
+                    self.job_title.append(job_title0)
+
+                    company_name0 = job.find_element(By.XPATH,
+                                                    f'/html/body/main/section/div[2]/div[2]/div[1]/div[{n}]/div[1]/div/div/p[1]').get_attribute(
+                        'innerText')
+                    self.company_name.append(company_name0)
+
+                    location0 = job.find_element(By.XPATH,
+                                                f'/html/body/main/section/div[2]/div[2]/div[1]/div[{n}]/div[1]/div/div/div[2]/span[1]').get_attribute(
+                        'innerText')
+                    self.location.append(location0)
+
+                    date0 = job.find_element(By.XPATH,
+                                            f'/html/body/main/section/div[2]/div[2]/div[1]/div[{n}]/div/p').get_attribute(
+                        'innerText')
+                    self.date.append(date0)
+
+                    job_link0 = job.find_element(By.XPATH,
+                                                f'/html/body/main/section/div[2]/div[2]/div[1]/div[{n}]/div[1]/div/div/div[1]/a').get_attribute(
+                        'href')
+                    self.job_link.append(job_link0)
+                    self.uuid.append(self.generate_uuid(job_link0))
+
+                    job_id0 = "Not available on Jobberman"
+                    self.job_id.append(job_id0)
+            except Exception as e:
+                logger.error(f"Error getting job {n} details: {e}")
+
+    def get_job_details(self, job_link: list, start_at: int):
+        if start_at == 0:
+            pass
+        else:
+            job_link = job_link[start_at:]
+
+        page = 0
+        for link in job_link:
+            page += 1
+            try:
+                # Load job page
+                wd = self.driver
+                wd.get(link)
+
+                try:
+                    # Get job description
+                    job_summary = wd.find_element(By.CSS_SELECTOR,
+                                                "#tab1 > div.flex.flex-col.rounded-lg.border-gray-300.md\:border.hover\:border-gray-400.md\:mx-0 > article > div:nth-child(4)").get_attribute(
+                        'innerText')
+                    job_req = wd.find_element(By.CSS_SELECTOR,
+                                            "#tab1 > div.flex.flex-col.rounded-lg.border-gray-300.md\:border.hover\:border-gray-400.md\:mx-0 > article > div:nth-child(5)").get_attribute(
+                        'innerText')
+                    job_description = str(job_summary) + "\n\n" + str(job_req)
+                    self.job_desc.append(job_description)
+                except:
+                    self.job_desc.append('NA')
+
+                try:
+                    # Seniority level
+                    seniority_level = wd.find_element(By.CSS_SELECTOR,
+                                                    '#tab1 > div.flex.flex-col.rounded-lg.border-gray-300.md\:border.hover\:border-gray-400.md\:mx-0 > article > div:nth-child(4) > ul > li:nth-child(2) > span.pb-1.text-gray-500').get_attribute(
+                        'innerText')
+                    self.seniority.append(seniority_level)
+                except:
+                    self.seniority.append('NA')
+
+                try:
+                    # Employment type
+                    employment_type = wd.find_element(By.CSS_SELECTOR,
+                                                    "#tab1 > div.flex.flex-col.rounded-lg.border-gray-300.md\:border.hover\:border-gray-400.md\:mx-0 > article > div.flex.flex-wrap.justify-start.pt-5.pb-2.px-4.w-full.border-b.border-gray-300.md\:flex-nowrap.md\:px-5 > div.w-full.text-gray-500 > div.mt-3 > span > a").get_attribute(
+                        'innerText')
+                    self.emp_type.append(employment_type)
+                except:
+                    self.emp_type.append('NA')
+
+                try:
+                    # Job function
+                    job_function = wd.find_element(By.CSS_SELECTOR,
+                                                "#tab1 > div.flex.flex-col.rounded-lg.border-gray-300.md\:border.hover\:border-gray-400.md\:mx-0 > article > div.flex.flex-wrap.justify-start.pt-5.pb-2.px-4.w-full.border-b.border-gray-300.md\:flex-nowrap.md\:px-5 > div.w-full.text-gray-500 > h2:nth-child(3) > a").get_attribute(
+                        'innerText')
+                    self.job_func.append(job_function)
+                except:
+                    self.job_func.append('NA')
+
+                try:
+                    # Job industry
+                    industries = wd.find_element(By.CSS_SELECTOR,
+                                                "#tab1 > div.flex.flex-col.rounded-lg.border-gray-300.md\:border.hover\:border-gray-400.md\:mx-0 > article > div.flex.flex-wrap.justify-start.pt-5.pb-2.px-4.w-full.border-b.border-gray-300.md\:flex-nowrap.md\:px-5 > div.w-full.text-gray-500 > div:nth-child(5) > a").get_attribute(
+                        'innerText')
+                    self.ind.append(industries)
+                except:
+                    self.ind.append('NA')
+
+            except Exception as e:
+                logger.error(f"There was an error scraping Page {page}: {e}")
                 self.job_desc.append("NA")
                 self.seniority.append("NA")
                 self.emp_type.append("NA")
